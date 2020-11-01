c0171c86bb4631c87bf0a44aa8a0eb209d5d4263
Mohit Arora <mohitarora.j2ee@gmail.com>
2020-11-01 17:01:45 +0530

    name-change fix

--- apps/accord/src/app/app.component.ts
+++ apps/accord/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'rebel.io-root',
+  selector: 'rebelio-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })

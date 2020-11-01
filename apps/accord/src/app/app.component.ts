2617ab343877437303b5d8fb45e12ceec08a8690
Mohit Arora <mohitarora.j2ee@gmail.com>
2020-11-01 15:08:12 +0530

    initial commit

---
+++ apps/accord/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rebel.io-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'accord';
+}

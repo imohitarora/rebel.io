ee50466d889a33374c43ea6a21c262405d9b449e
Mohit Arora <mohitarora.j2ee@gmail.com>
2020-11-02 10:03:53 +0530

    Chepter 1 2

--- apps/accord-e2e/src/support/app.po.ts
+++ apps/accord-e2e/src/support/app.po.ts
@@ -1,1 +1,2 @@
-export const getGreeting = () => cy.get('h1');
+export const getAccords = () => cy.get('li.accord');
+export const getAddAccordButton = () => cy.get('button#add-accord');
--- apps/accord/src/app/app.component.ts
+++ apps/accord/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Accord {
+  title: string;
+}
+
 @Component({
   selector: 'rebelio-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'accord';
+  accords: Accord[] = [{ title: 'Acc 1' }, { title: 'Acc 1' }];
+
+  addAccord() {
+    this.accords.push({
+      title: `New Accord ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
 }
